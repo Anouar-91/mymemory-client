badde6359c2565e6d89b04dfbfbaca18d34603a8
Anouar-91 <a.berrouane@hotmail.com>
2022-10-16 17:02:01 +0200

    init project

---
+++ src/assets/style.js
@@ -0,0 +1,4 @@
+// any CSS you require will output into a single css file (app.css in this case)
+require('./elems/index.scss');
+require('bootstrap');
+
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function HomePage() {
+    return (
+        <div className="p-5 mb-4 bg-light rounded-3">
+            <div class="container-fluid py-5">
+                <h1 class="display-5 fw-bold">MyMemory</h1>
+                <p class="col-md-8 fs-4">MyMemory is an web application developed to work on memory and English. </p>
+                <button class="btn btn-primary btn-lg" type="button">Example button</button>
+            </div>
+        </div>
+    )
+}
+
+export default HomePage
