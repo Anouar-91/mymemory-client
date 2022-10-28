1422e53d92931992c648a9a195b110cabc2c9918
Anouar-91 <a.berrouane@hotmail.com>
2022-10-28 16:31:12 +0200

    update home page

--- src/pages/ChoiceTypeQuizPage.jsx
+++ src/pages/ChoiceTypeQuizPage.jsx
@@ -1,37 +1,53 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import enWordQuestion from '../assets/img/enWordQuestion.png'
+import frWordQuestion from '../assets/img/frWordQuestion.png'
 
 function ChoiceTypeQuizPage() {
     return (
         <>
-            <h1 className="text-center">Choisis un mode : </h1>
-            <div className="row mt-5 justify-content-center">
-                <div className="col-md-4 col-6">
-                    <div className="card mb-3" >
-                    <div className="card-header text-primary text-center h4"><strong>English to french</strong></div>
-                        <div className="card-body">
-                            <h5 className="card-title">Rules</h5>
-                            <p className="card-text">In this mode you have to translate english words into french.</p>
+            <div className="container">
+                <div className="title-primary mt-3 mb-4">
+                    Quiz Word : Choose a mode
+                </div>
+                <div className="row">
+                    <div className="col-md-6 mt-3">
+                        <div className="position-relative">
+
                         </div>
-                        <div className="card-footer text-center">
+                        <div className="card-shadow text-center">
+                            <h2>English to french</h2>
+                            <div className="d-flex align-items-center">
+                                <div className="col-3">
+                                    <img src={enWordQuestion} alt="illustration" className="img-fluid w-75" />
+                                </div>
+                                <div className="col-9">
+                                    <p>In this mode you have to translate english words into french.</p>
+                                </div>
+                            </div>
                             <Link to="/quiz_en_words" className="btn btn-primary">Select</Link>
+
                         </div>
                     </div>
-                </div>
-                <div className="col-md-4 col-6">
-                    <div className="card mb-3" >
-                        <div className="card-header text-primary text-center h4"><strong>French to english</strong></div>
-                        <div className="card-body">
-                            <h5 className="card-title">Rules</h5>
-                            <p className="card-text">In this mode you have to translate french words into english.</p>
-                        </div>
-                        <div className="card-footer text-center">
-                        <Link to="/quiz_fr_words" className="btn btn-primary">Select</Link>
+                    <div className="col-md-6 mt-3">
+                        <div className="card-shadow text-center">
+                            <h2>French to french</h2>
+                            <div className="d-flex align-items-center">
+                                <div className="col-3">
+                                    <img src={frWordQuestion} alt="illustration" className="img-fluid w-75" />
+                                </div>
+                                <div className="col-9">
+                                    <p>In this mode you have to translate french words into english.</p>
+                                </div>
+                            </div>
+                            <Link to="/quiz_fr_words" className="btn btn-primary">Select</Link>
+
                         </div>
                     </div>
                 </div>
 
             </div>
+
         </>
     )
 }
