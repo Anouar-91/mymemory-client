ce6792d8f214e0c6224a5deaa97913cfc90b14a9
Anouar-91 <a.berrouane@hotmail.com>
2022-10-28 15:28:03 +0200

    update home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -8,11 +8,15 @@ function HomePage() {
         <div className="p-5 bg-primary home-bg">
             <div className="container py-5">
                 <div className="row align-items-center">
-                    <div className="col-md-7">
+                    <div className="col-md-7 ">
                         <img src={mymemory} alt="" className="img-fluid" />
                         <p className="col-md-8 fs-4 text-light">MyMemory is an web application developed to work on memory and English. </p>
-                        <Link to="/login" className="btn btn-primary btn-lg" >Sign In</Link>&nbsp;&nbsp;
-                        <Link to="/register" className="btn btn-secondary btn-lg" >Sign up</Link>
+                        <div className="d-flex">
+                            <div className="col-md-3 mr-5 mr-md-0"><Link to="/login" className="btn btn-primary btn-lg" >Sign In</Link></div>
+                            <div className="col-md-3"><Link to="/register" className="btn btn-secondary btn-lg" >Sign up</Link></div>
+                        </div>
+                        
+                        
                     </div>
                     <div className="col-md-5">
                         <img src={homeIllustration} alt="" className="img-fluid" />
