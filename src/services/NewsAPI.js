c7a9e8f91e711de5ab6a389bfbbb2f50869053ab
Anouar-91 <a.berrouane@hotmail.com>
2022-11-15 12:13:28 +0100

    design new card

--- src/services/NewsAPI.js
+++ src/services/NewsAPI.js
@@ -4,7 +4,7 @@ import {API_URL} from './Config'
 
 async function findAll(){
 
-    return axios.get(API_URL + 'news?order[id]')
+    return axios.get(API_URL + '?order[createdAt]')
       .then(response =>{
         const news = response.data["hydra:member"];
         return news
