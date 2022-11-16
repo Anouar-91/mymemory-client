b18860b61eab75ddcedee4404f45255bff92a0e2
Anouar-91 <a.berrouane@hotmail.com>
2022-11-16 10:54:56 +0100

    add loading in page news

--- src/pages/NewsPage.jsx
+++ src/pages/NewsPage.jsx
@@ -1,15 +1,21 @@
 import React, { useEffect, useState } from 'react'
 import NewsAPI from '../services/NewsAPI';
 import CardNews from '../components/CardNews';
+import { ThreeDots } from 'react-loader-spinner';
+import { toast } from 'react-toastify';
 
 const NewsPage = () => {
 
   const [news, setNews] = useState([])
+  const [loading, setLoading] = useState(true);
   const fetchNews = async () => {
     try {
       const data = await NewsAPI.findAll();
       setNews(data);
+      setLoading(false)
     } catch (error) {
+      setLoading(false)
+      toast.error("An error occurred while loading news")
 
     }
   }
@@ -23,13 +29,24 @@ const NewsPage = () => {
       <div className="title-primary mt-3 mb-4">
         News
       </div>
-      {
+      {loading ? 
+                      <div className="text-center">
+                      <ThreeDots
+                          color="#C30028"
+                          wrapperStyle={{ justifyContent: 'center' }}
+                      />
+                  </div>
+      :(
+     
         news.map((news) => {
           return (
             <CardNews news={news}/>
           )
         })
-      }
+      
+      )
+    }
+ 
 
     </div>
   )
